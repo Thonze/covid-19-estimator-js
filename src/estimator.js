a79f6ee528b99e2402331f78a45ee5b970143b5a
Mobulus <58326369+Thonze@users.noreply.github.com>
2020-04-09 13:52:45 +0100

    Revert "Accessment"

--- src/estimator.js
+++ src/estimator.js
@@ -1,29 +1,3 @@
-const covid19ImpactEstimator = (data) => {
-    const input = data;
-    return{
-        data:{
-             region: {
-                 name: "Africa",
-                 avgAge: 19.7,
-                 avgDailyIncomeInUSD: 5,
-                 avgDailyIncomePopulation: 0.71
-             },
-             factor="numbers",
-             periodType:"days",
-             timeToElapse: 58,
-             reportedCases: 674,
-             population: 66622705,
-             totalHospitalBeds: 1380614
-                },
-        impact: {
-             currentlyInfected = reportedCases * 10,
-             infectionsByRequestedTime = currentlyInfected * Math.pow(2,factor)
-        },
-        severeImpact:{
-            currentlyInfected= reportedCases * 50,
-            infectionsByRequestedTime = currentlyInfected * Math.pow(2,factor) 
-        }
-    }
-};
+const covid19ImpactEstimator = (data) => data;
 
 export default covid19ImpactEstimator;
